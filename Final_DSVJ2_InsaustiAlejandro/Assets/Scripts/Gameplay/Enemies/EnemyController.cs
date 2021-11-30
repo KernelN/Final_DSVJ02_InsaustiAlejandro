4e7a0df7bc8fcd7e43e1d1b71b6ca946d70facdb
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2021-11-30 17:42:36 -0300

    Add Enemy

---
+++ Final_DSVJ2_InsaustiAlejandro/Assets/Scripts/Gameplay/Enemies/EnemyController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] LayerMask obstacleLayers;
+    [SerializeField] LayerMask hittableLayers;
+    [SerializeField] float speed;
+
+    //Unity Events
+    private void Update()
+    {
+        Move();
+    }
+
+    //Methods
+    void Move()
+    {
+        Vector3 movement = GetMovement();
+        if (movement.z == 0) return;
+        if (MovementBlocked(movement)) return;
+        HitObjects(movement);
+        transform.Translate(movement);
+    }
+    Vector3 GetMovement()
+    {
+        return new Vector3(0, 0, speed * Time.deltaTime);
+    }
+    bool MovementBlocked(Vector3 direction)
+    {
+        Vector3 pos = transform.position + direction;
+        Vector3 radius = transform.localScale / 2;
+        Quaternion rotation = transform.rotation;
+        return Physics.OverlapBox(pos, radius, rotation, obstacleLayers).Length > 0;
+    }
+    void HitObjects(Vector3 direction)
+    {
+        Vector3 pos = transform.position + direction; //remove direction? could collide before expected
+        Vector3 radius = transform.localScale / 2;
+        Quaternion rotation = transform.rotation;
+        Collider[] colls = Physics.OverlapBox(pos, radius, rotation, hittableLayers);
+        IHittable hit;
+        foreach (Collider col in colls)
+        {
+            hit = col.GetComponent<IHittable>();
+            if (hit == null) continue; //if hitted component doesn't have IHittable, skip
+            hit.GetHitted();
+        }
+    }
+}
